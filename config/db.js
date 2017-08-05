f17fc56c69cb3c928e575c6da023d4350d1c4632
rthakur <mailtorkthakur@gmail.com>
2017-08-05 17:46:02 +0530

    Changed DB name

--- config/db.js
+++ config/db.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 //var connString = 'mongodb://localhost:27017/people';
-var connString = 'mongodb://rakesh:rkt123@ds131583.mlab.com:31583/people';
+var connString = 'mongodb://moraletracker:rkt123@ds131583.mlab.com:31583/rakeshthakur';
 module.exports = ObjRepo.db = {
     connectionString: connString,
     connectionObj: mongoose.createConnection(connString),
