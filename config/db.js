3990a69b20cb1ce98da5cfe302b24d3e89bcc7ff
rthakur <mailtorkthakur@gmail.com>
2017-08-05 16:17:14 +0530

    removed collect folder

--- config/db.js
+++ config/db.js
@@ -1,5 +1,6 @@
 var mongoose = require('mongoose');
-var connString = 'mongodb://localhost:27017/people';
+//var connString = 'mongodb://localhost:27017/people';
+var connString = 'mongodb://rakesh:rkt123@ds131583.mlab.com:31583/people';
 module.exports = ObjRepo.db = {
     connectionString: connString,
     connectionObj: mongoose.createConnection(connString),
